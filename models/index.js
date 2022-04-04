4ed5252a1f263921d866c773c9125f4bc0925cef
Tyraine <danogaryjardine@gmail.com>
2022-04-04 13:43:28 -0600

    Adding the Models to the project

---
+++ models/index.js
@@ -0,0 +1,19 @@
+const Post = require('./Post');
+const User = require('./User');
+const Comment = require('./Comment');
+
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
